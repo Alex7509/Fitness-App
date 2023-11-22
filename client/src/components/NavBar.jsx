fd34240834b9e60fe9a21be0b988c584e2dd7336
Alex Georgiev <alex_georgiev_03@abv.bg>
2023-11-22 16:22:24 +0200

    Change navigation bar if thereis a User
    Show User username on navigation bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,39 +1,31 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 
+import { AuthContext } from "../contexts/authContext";
+
+
 export const NavBar = () => {
+    const { isAuth, username } = useContext(AuthContext);
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <Link className="navbar-brand" to="/">
-                    Home
-                </Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon" />
-                </button>
+                {isAuth && (
+                    <span class=" text-white">
+                        Welcome {username}
+                    </span>
+                )}
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/login">
-                                Login
+                            <Link className="nav-link" to="/">
+                                Home
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/register">
-                                Register
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/logout">
-                                Logout
+                            <Link className="nav-link" to="/about">
+                                About
                             </Link>
                         </li>
                         <li className="nav-item">
@@ -41,25 +33,43 @@ export const NavBar = () => {
                                 Catalog
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/add-exercise">
-                                Add exercises
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                My program
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/about">
-                                About
-                            </Link>
-                        </li>
+                        {!isAuth && (
+                            <>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/login">
+                                        Login
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/register">
+                                        Register
+                                    </Link>
+                                </li>
+                            </>
+                        )}
 
+                        {isAuth && (
+                            <>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/add-exercise">
+                                        Add exercises
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="#">
+                                        My program
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/logout">
+                                        Logout
+                                    </Link>
+                                </li>
+                            </>
+                        )}
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav >
     );
 };
