50c20aa1a9e1d53ff8214047490627f7440640ed
Alex Georgiev <alex_georgiev_03@abv.bg>
2023-12-02 10:57:06 +0200

    Add error handling and data validation to register form

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -13,62 +13,62 @@ export const Register = () => {
     });
 
     return (
-        <div className="conteiner min-vh-100 d-flex justify-content-center align-items-center form-control-lg">
-            <form onSubmit={onSubmit}>
-                <div className="form-group">
-                    <label htmlFor="exampleInputUsername1">Username</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="exampleInputUsername1"
-                        placeholder="Username"
-                        name="username"
-                        value={values.username}
-                        onChange={onChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input
-                        type="email"
-                        className="form-control"
-                        id="exampleInputEmail1"
-                        placeholder="Enter email"
-                        name="email"
-                        value={values.email}
-                        onChange={onChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        id="exampleInputPassword1"
-                        placeholder="Password"
-                        name="password"
-                        value={values.password}
-                        onChange={onChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Confirm password</label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        id="exampleInputPassword2"
-                        placeholder="Confirm password"
-                        name="confirmPassword"
-                        value={values.confirmPassword}
-                        onChange={onChange}
-                    />
-                </div>
-                <div className="text-center form-control-lg">
-                    <button type="submit" className="btn btn-primary">
-                        Register
-                    </button>
-                </div>
-            </form>
-        </div>
+            <div className="conteiner min-vh-100 d-flex justify-content-center align-items-center form-control-lg">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputUsername1">Username</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="exampleInputUsername1"
+                            placeholder="Username"
+                            name="username"
+                            value={values.username}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Email address</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            placeholder="Enter email"
+                            name="email"
+                            value={values.email}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            placeholder="Password"
+                            name="password"
+                            value={values.password}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Confirm password</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="exampleInputPassword2"
+                            placeholder="Confirm password"
+                            name="confirmPassword"
+                            value={values.confirmPassword}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="text-center form-control-lg">
+                        <button type="submit" className="btn btn-primary">
+                            Register
+                        </button>
+                    </div>
+                </form>
+            </div>
     );
 };
--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify"
 
 import * as authService from "../services/authService";
 
@@ -22,23 +23,45 @@ export const AuthProvider = ({ children }) => {
         localStorage.setItem('accessToken', result.accessToken);
 
         navigate('/');
+
+
     };
 
     const registerSubmitHandler = async (values) => {
         if (values.password !== values.confirmPassword) {
-            return;
+            return toast.error('Passwords missmatch');
+        }
+
+        if (values.username === '' ||
+            values.email === '' ||
+            values.password === '' ||
+            values.confirmPassword === '') {
+            toast.error('All fields are required');
+        }
+
+        if (values.username.length < 4) {
+            toast.error('Username must be 4 characters long')
+        }
+
+        if (values.password.length < 5) {
+            toast.error('Password must be 5 characters long')
         }
 
         const result = await authService.register(values.username, values.email, values.password);
 
-        setAuth(result);
+        try {
+            setAuth(result);
 
-        localStorage.setItem('accessToken', result.accessToken);
+            localStorage.setItem('accessToken', result.accessToken);
 
-        navigate('/');
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+        }
+
     };
 
-    const logoutHandler = () => {
+    const logoutHandler = (e) => {
         setAuth({});
 
         localStorage.removeItem('accessToken');
@@ -54,6 +77,8 @@ export const AuthProvider = ({ children }) => {
         username: auth.username,
         email: auth.email,
         isAuth: !!auth.username,
+        userData: auth,
+        token: auth.accessToken,
     };
 
     return (
