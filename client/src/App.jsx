1577b3b172e3b69d80832d47b35961a9eed360f4
Alex Georgiev <alex_georgiev_03@abv.bg>
2023-12-02 12:23:37 +0200

    Add 404 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,27 +11,31 @@ import { Details } from "./components/Details/Details";
 import { Exercises } from "./components/Exercises/Exercises";
 import { Edit } from "./components/Edit/Edit";
 import { MyProgram } from "./components/MyProgram/MyProgram";
+import { ErrorPage } from "./components/404Page/404Page";
+
 
 import { AuthProvider } from "./contexts/authContext";
+
 
 function App() {
     return (
         <AuthProvider>
-                <>
-                    <NavBar />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/logout" element={<Logout />} />
-                        <Route path="/exercises" element={<Exercises />} />
-                        <Route path="/add-exercise" element={<AddExercise />} />
-                        <Route path="/exercises/:exerciseId/details" element={<Details />} />
-                        <Route path="/exercises/:exerciseId/edit" element={<Edit />} />
-                        <Route path="/my-program" element={<MyProgram />} />
-                    </Routes>
-                </>
+            <>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/logout" element={<Logout />} />
+                    <Route path="/exercises" element={<Exercises />} />
+                    <Route path="/add-exercise" element={<AddExercise />} />
+                    <Route path="/exercises/:exerciseId/details" element={<Details />} />
+                    <Route path="/exercises/:exerciseId/edit" element={<Edit />} />
+                    <Route path="/my-program" element={<MyProgram />} />
+                    <Route path="/*" element={<ErrorPage />} />
+                </Routes>
+            </>
         </AuthProvider>
     );
 };
