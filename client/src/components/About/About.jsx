7d4cb639f88770b9fff31ef74949d8ac301cef57
Alex Georgiev <alex_georgiev_03@abv.bg>
2023-12-10 22:04:13 +0200

    Update about page text

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -3,8 +3,9 @@ export const About = () => {
         <>
             <img src="https://hips.hearstapps.com/hmg-prod/images/chris-64a6d63b7263d.png?crop=1.00xw:0.963xh;0,0.0372xh&resize=1200:*" className="rounded float-end" alt="Chris Bumstead" />
             <div className="text-center">
+                <h1>Exercise & Fitness</h1>
                 <p className="text-xl-left">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia autem veniam vero odit reiciendis alias ut, deleniti, voluptatum consectetur cupiditate commodi, molestias rem dolores doloribus exercitationem quidem modi eum consequuntur?
+                    Exercising regularly, every day if possible, is the single most important thing you can do for your health. In the short term, exercise helps to control appetite, boost mood, and improve sleep. In the long term, it reduces the risk of heart disease, stroke, diabetes, dementia, depression, and many cancers.
                 </p>
             </div>
         </>
